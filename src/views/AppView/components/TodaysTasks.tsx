8e399b58b79d5f93e0fcca78167061262ae9594d
saurabhburade <sburade3@gmail.com>
2024-02-18 03:59:45 +0530

    chore: remove unwanted code

--- src/views/AppView/components/TodaysTasks.tsx
+++ src/views/AppView/components/TodaysTasks.tsx
@@ -1,17 +1,9 @@
 import CheckBox from "@/components/CheckBox";
 import React, { useState } from "react";
-import {
-  motion,
-  HTMLMotionProps,
-  useMotionValue,
-  useTransform,
-} from "framer-motion";
 import { DEFAULT_TASKS } from "@/configs/constants";
 type Props = {};
 
 function TodaysTasks({}: Props) {
-  //   const [checkList, setcheckList] = useState(TASKS);
-
   return (
     <div className="bg-base-300 p-[32px] rounded-xl space-y-[24px]  h-full  max-w-[400px] min-w-fit">
       <h2 className="text-[24px] font-[600]">Today{"'"}s Tasks</h2>
