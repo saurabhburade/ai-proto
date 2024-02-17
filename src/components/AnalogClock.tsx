8f0b5883c51a4c7381ce1e24734a3400dba6e024
saurabhburade <sburade3@gmail.com>
2024-02-18 04:17:18 +0530

    chore: add clock markers

--- src/components/AnalogClock.tsx
+++ src/components/AnalogClock.tsx
@@ -14,7 +14,12 @@ const AnalogClock = () => {
   } = useClockTime();
   useEffect(() => {});
   return (
-    <div className="analog-clock  bg-white ">
+    <div className="analog-clock  bg-white relative top-0">
+      <span className="w-[13.33px] border border-[##9E9FA6] absolute top-[50%] -translate-y-[50%] left-[8px] bg-[##9E9FA6] "></span>
+      <span className="h-[13.33px] border border-[##9E9FA6] absolute left-[50%] -translate-x-[50%] top-[8px] bg-[##9E9FA6] "></span>
+      <span className="w-[13.33px] border border-[##9E9FA6] absolute top-[50%] -translate-y-[50%] right-[8px] bg-[##9E9FA6] "></span>
+      <span className="h-[13.33px] border border-[##9E9FA6] absolute left-[50%] -translate-y-[50%] bottom-[8px] bg-[##9E9FA6]"></span>
+
       <div
         className="hand hour-hand"
         style={{ transform: `rotate(${hourDeg}deg)` }}
